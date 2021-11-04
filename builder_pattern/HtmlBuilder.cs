ff350c99ffe0f7a4dbe910c5c3f95a9dc0c8e773
mohamadreza hedayat <mohamadrezahedayat@gmail.com>
2021-11-04 13:26:31 +0330

    add fluent builder and refactor

---
+++ builder_pattern/HtmlBuilder.cs
@@ -0,0 +1,31 @@
+﻿namespace DesignPatterns
+{
+    public class HtmlBuilder
+    {
+        private readonly string _rootName;
+        private HtmlElement _root = new HtmlElement();
+
+        public HtmlBuilder(string rootName)
+        {
+            _rootName = rootName;
+            _root.Name = rootName;
+        }
+
+        public HtmlBuilder AddChild(string childName, string childText)
+        {
+            var e = new HtmlElement(childName, childText);
+            _root.Elements.Add(e);
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return _root.ToString();
+        }
+
+        public void Clear()
+        {
+            _root = new HtmlElement {Name = _rootName};
+        }
+    }
+}
---
+++ builder_pattern/HtmlElement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns
+{
+    public class HtmlElement
+    {
+        public string Name, Text;
+        public List<HtmlElement> Elements = new List<HtmlElement>();
+        private const int IndentSize = 2;
+
+        public HtmlElement(string name, string text)
+        {
+            Name = name?? throw new ArgumentNullException(paramName:nameof(name));
+            Text = text ?? throw new ArgumentNullException(paramName: nameof(text));
+        }
+
+        public HtmlElement() { }
+        private string ToStringImpl(int indent)
+        {
+            var sb = new StringBuilder();
+            var i = new String(' ', IndentSize * indent);
+
+            sb.AppendLine($"{i}<{Name}>");
+            if (!String.IsNullOrWhiteSpace(Text))
+            {
+                sb.Append(new String(' ', IndentSize * indent + 1));
+                sb.AppendLine(Text);
+            }
+
+            foreach (var e in Elements)
+            {
+                sb.Append(e.ToStringImpl(indent + 1));
+            }
+            sb.AppendLine($"{i}</{Name}>");
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToStringImpl(0);
+        }
+    }
+}
