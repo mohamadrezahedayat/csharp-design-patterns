0591ec7e9cbcee2c3f501d1fd759ad2b46d1d401
mohamadreza hedayat <mohamadrezahedayat@gmail.com>
2021-11-05 21:42:55 +0330

    add Faceted Builder

---
+++ Faceted Builder/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace Faceted_Builder
+{
+    public class Person
+    {
+        // address
+        public string StreetAddress, Postcode, City;
+
+        // employment
+        public string CompanyName, Position;
+        public int AnnualIncome;
+
+        public override string ToString()
+        {
+            return  $"{nameof(City)} : {City}\n" +
+                    $"{nameof(StreetAddress)} : {StreetAddress}\n" +
+                    $"{nameof(Postcode)} : {Postcode}\n" +
+                    $"{nameof(CompanyName)} : {CompanyName}\n" +
+                    $"{nameof(Position)} : {Position}\n" +
+                    $"{nameof(AnnualIncome)} : {AnnualIncome}";
+        }
+
+    }
+}
---
+++ Faceted Builder/PersonAddressBuilder.cs
@@ -0,0 +1,27 @@
+﻿namespace Faceted_Builder
+{
+    public class PersonAddressBuilder : PersonBuilder
+    {
+        // might not work with a value type like struct because it is reference!
+        public PersonAddressBuilder(Person person)
+        {
+            Person = person;
+        }
+
+        public PersonAddressBuilder At(string streetAddress)
+        {
+            Person.StreetAddress = streetAddress;
+            return this;
+        }
+        public PersonAddressBuilder WithPostcode(string postcode)
+        {
+            Person.Postcode = postcode;
+            return this;
+        }
+        public PersonAddressBuilder In(string city)
+        {
+            Person.City = city;
+            return this;
+        }
+    }
+}
