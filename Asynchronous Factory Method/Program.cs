e4514eafc95e8e7e2cd3766be86da15596c15091
mohamadreza hedayat <mohamadrezahedayat@gmail.com>
2021-11-07 15:39:26 +0330

    Add Asynchronous Factory Method

---
+++ Asynchronous Factory Method/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Asynchronous_Factory_Method
+{
+    public class Foo
+    {
+        private Foo() { /* constructor is private */}
+
+        private async Task<Foo> InitAsync()
+        {
+            await Task.Delay(1000);
+            return this;
+        }
+
+        public static Task<Foo> CreateAsync()
+        {
+            var result = new Foo();
+            return result.InitAsync();
+        }
+    }
+    class Program
+    {
+        static async Task Main()
+        {
+            Foo x = await Foo.CreateAsync();
+            Console.ReadLine();
+        }
+    }
+}
